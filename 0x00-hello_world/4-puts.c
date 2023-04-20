dca038e35a80ab3a44594aeadabcf4a0d07783be
Thekorede <Thekorede@outlook.com>
2023-04-20 16:56:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-1 #include <stdio.h>
-2 /**
-3  * main - Entry point
-4  *
-5  *Returns: Always O (Success)
-6 */
-7 int main(void)
-8 {
-9          puts("\"Programming is like building a multilingual puzzle");
-10      return (O);
-11 }
+ #include <stdio.h>
+ /**
+  * main - Entry point
+  *
+  *Returns: Always O (Success)
+ */
+ int main(void)
+ {
+	puts("\"Programming is like building a multilingual puzzle");
+      return (0);
+  }
