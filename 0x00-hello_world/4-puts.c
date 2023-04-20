f63ea57f9c3295d2e7e0980801ca78b37ce6f458
Thekorede <Thekorede@outlook.com>
2023-04-20 13:51:53 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #!/bin/bash
 #include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Alway O (success)
+*/
+int main(void)
+{
+          puts("\"Programming is like building a multilingual puzzle
+"):
+       Return(O):
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
 }
-
