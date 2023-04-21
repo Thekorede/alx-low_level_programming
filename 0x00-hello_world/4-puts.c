435e7df8dcf0d79c6d690495b98dc092bad99801
Thekorede <Thekorede@outlook.com>
2023-04-20 17:13:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
- #include <stdio.h>
- /**
-  * main - Entry point
-  *
-  *Returns: Always O (Success)
+#include <stdio.h>
+/**
+ * main - A program that print a line with puts function
+ *
+ *Returns: Always O (Success)
  */
- int main(void)
- {
-	puts("\"Programming is like building a multilingual puzzle");
-      return (0);
-  }
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
